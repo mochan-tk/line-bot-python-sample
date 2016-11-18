c1ff9ea8545da9fb451cfb21ed01835f98f585ce
mochan-tk <takashi1.kawamoto@gmail.com>
2016-11-19 01:34:37 +0900

    かろうじて動作する程度の能力(Bot)

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from gae_http_client import RequestsHttpClient
+
+from google.appengine.api import taskqueue
+
 from google.appengine.ext import vendor
 vendor.add('lib')
+
 from flask import Flask, request, abort
 
 from linebot import (
@@ -16,7 +24,7 @@ import config
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi(config.CHANNEL_ACCESS_TOKEN )
+line_bot_api = LineBotApi(config.CHANNEL_ACCESS_TOKEN, http_client=RequestsHttpClient)
 handler = WebhookHandler(config.CHANNEL_SECRET)
 
 
@@ -28,6 +36,19 @@ def callback():
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
+
+    # Task Queue Add
+    taskqueue.add(url='/worker',
+                  params={'body': body,
+                          'signature': signature},
+                  method="POST")
+
+    return 'OK'
+
+@app.route("/worker", methods=['POST'])
+def worker():
+    body = request.form.get('body')
+    signature = request.form.get('signature')
 
     # handle webhook body
     try:
@@ -43,3 +64,7 @@ def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text='僕と契約して、魔法少女になってよ！'))
+
+
+if __name__ == "__main__":
+    app.run()
